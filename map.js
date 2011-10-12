59ba8a24e51fc40da15fac10c9879491391456a0
Stewart Wilson <wilsons12@up.edu>
2011-10-12 01:02:10 -0700

    added another zip that can be displayed and enabled a click listener

--- map.js
+++ map.js
@@ -7,12 +7,12 @@
 function initialize(){
 
         // Initialize map information
-
+	var washZipCodes = ['98295', '98177'];
         // The center point of the map is Salem, OR
     var latlng = new google.maps.LatLng(0.477436023333333E+02,-0.122369343004934E+03);
 
     var myOptions = {
-      zoom: 10,
+      zoom: 5,
       center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
@@ -20,29 +20,38 @@ function initialize(){
         // Draw the map using the options defined above.
     var map = new google.maps.Map(document.getElementById("map_canvas"),
         myOptions);
-            
+           
+    var zipCodeAreas = new Array();
     // Create a marker at the initial center position of the map.
     // The "title" is what is displayed when the user hovers over 
     // the marker.  This is called a Google Maps "tooltip".
         var marker = new google.maps.Marker({
       position: latlng, 
       map: map, 
-      title:"Seattle, WA"
+      title:"Washington"
    });
 
         // Create the polygon that outlines zipcode 98177
         // Call the function constructZipCodeArray() to 
         // get the path of geopgraphical points.
-        zipCodeArea = new google.maps.Polygon({
-        paths: constructZipCodeArray(),
+        for(i = 0; i < 2; i++)
+        {
+			zipCodeAreas[i] = new google.maps.Polygon({
+        paths: zips[washZipCodes[i]],
         strokeColor: "#a020f0",
-        strokeOpacity: 0.8,
+        strokeOpacity: 0.35,
         strokeWeight: 2,
         fillColor: "#a020f0",
-        fillOpacity: 0.35
-    });
-
-  // Draw the polygon on the map.
-  zipCodeArea.setMap(map);
+        fillOpacity: 0.35});
+		
+		                  
+        google.maps.event.addListener(zipCodeAreas[i], 'click', function(event) {       
+                        console.log("Hi " + washZipCodes[i] + ' ' + "!"); 
+                        
+        }); 
+		
+		zipCodeAreas[i].setMap(map);
+		
+		}
     
 }
